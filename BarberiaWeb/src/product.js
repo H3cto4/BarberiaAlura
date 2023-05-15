28d799b12d63b546633424fcc88b2382e0c40ffa
H3cto4 <pirlo.hf@gmail.com>
2023-05-14 20:52:54 -0500

    commit changes links

--- BarberiaWeb/src/product.js
+++ BarberiaWeb/src/product.js
@@ -4,10 +4,12 @@ lis.forEach(li => {
   li.addEventListener('mouseover', () => {
     li.style.borderColor = 'gold';
     li.style.boxShadow = '0 0 10px gold';
+    li.style.transform = 'scale(1.05)';
   });
 
   li.addEventListener('mouseout', () => {
     li.style.borderColor = 'black';
     li.style.boxShadow = 'none';
+    li.style.transform = 'scale(1)';
   });
 });
